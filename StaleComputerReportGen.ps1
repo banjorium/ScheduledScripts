e0d4cb7942de841c0acff5c4a553a8489087a940
Banjorium <jlafontaine.it@gmail.com>
2016-02-23 09:50:22 -0500

    Created "Gather-Computers" Function, Gets AD Computers with logontimestamp and pwdLastSet less than our time.

--- StaleComputerReportGen.ps1
+++ StaleComputerReportGen.ps1
@@ -40,3 +40,25 @@ $DS = "OU=Datatel Servers,OU=SNHU Servers,DC=snhu,DC=edu"
 $IS = "OU=Infrastructure Servers,OU=SNHU Servers,DC=snhu,DC=edu"
 $SQLS = "OU=SQL Servers,OU=SNHU Servers,DC=snhu,DC=edu"
 $EMAILS = "OU=E-Mail Servers,OU=SNHU Servers,DC=snhu,DC=edu"
+
+
+function Gather-Computers
+
+{
+
+#Get all AD computers with lastLogonTimestamp and pwdLastSet less than our time 
+Get-ADComputer -Filter {LastLogonTimeStamp -lt $lastLogonTimeStampDateFormat -and pwdLastSet -lt $pwdLastSetDateFormat} -Properties LastLogonTimeStamp, DistinguishedName |
+
+#Filter Out any Objects Living in these OUs: 
+?{
+    ($_.distinguishedname -notmatch $VDI) -and
+    ($_.distinguishedName -notmatch $SS) -and
+    ($_.distinguishedName -notmatch $ES) -and
+    ($_.distinguishedName -notmatch $AS) -and
+    ($_.distinguishedName -notmatch $DS) -and
+    ($_.distinguishedName -notmatch $IS) -and
+    ($_.distinguishedName -notmatch $SQLS) -and
+    ($_.distinguishedName -notmatch $EMAILS)
+  
+ } 
+} 
